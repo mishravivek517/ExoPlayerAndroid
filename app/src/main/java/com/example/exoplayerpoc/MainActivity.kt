2e8eda4f1c095e79916f5c5d2cea0787021bd0b5
mishravivek517 <mishravivek517@gmail.com>
2023-02-16 16:23:45 +0530

    Added player to xml, Streaming url to exo, Player configs

--- app/src/main/java/com/example/exoplayerpoc/MainActivity.kt
+++ app/src/main/java/com/example/exoplayerpoc/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.exoplayerpoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exoplayerpoc.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSource
+import com.google.android.exoplayer2.util.MimeTypes
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var player: ExoPlayer? = null
+    private val isPlaying get() = player?.isPlaying ?: false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+//        setContentView(R.layout.activity_main)
+        initializeExoPlayer()
     }
+
+    private fun initializeExoPlayer() {
+        player = ExoPlayer.Builder(this) // <- context
+            .build()
+
+        // create a media item.
+        val mediaItem = MediaItem.Builder()
+            .setUri("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+            .setMimeType(MimeTypes.APPLICATION_MP4)
+            .build()
+
+        // Create a media source and pass the media item
+        val mediaSource = ProgressiveMediaSource.Factory(
+            DefaultDataSource.Factory(this) //
+        )
+            .createMediaSource(mediaItem)
+
+        // Finally assign this media source to the player
+        player?.apply {
+            setMediaSource(mediaSource)
+            playWhenReady = true // start playing when the exoplayer has setup
+            seekTo(0, 0L) // Start from the beginning
+            prepare() // Change the state from idle.
+        }.also {
+            // Do not forget to attach the player to the view
+            binding.playerView.player = it
+        }
+    }
+
+
 }
